34bd4632f38b61cc3b310d5ec825103b25caa9e8
madihill07 <madigracehill@gmail.com>
2023-01-11 20:42:55 -0700

    commit all

---
+++ week01/scripts/main.js
@@ -0,0 +1,13 @@
+let date = new Date();
+
+console.log(date);
+
+let currentdate = document.lastModified;
+
+console.log(currentdate);
+
+let year = date.getFullYear();
+
+console.log(year);
+
+document.querySelector('.year').textContent = year;
