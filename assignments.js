5b7c84fa3a88485db12ca7928222b47db935a12a
madihill07 <madigracehill@gmail.com>
2023-02-12 00:21:26 -0700

    commit all

---
+++ assignments.js
@@ -0,0 +1,35 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function geAssignmentDatat(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayAssignments(data.assignments); 
+  }
+  
+  getAssignmentData(url);
+
+  const displayAssignments = (assignments) => {
+    assignments.forEach((assignment) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let week = document.createElement('h2'); // fill in the blank
+      let assignment1 = document.createElement('p');
+      let assignment2 = document.createElement('p');
+      let assignment3 = document.createElement('p');
+      let assignment4 = document.createElement('p');
+      let assignment5 = document.createElement('p');
+
+  
+      // Append the section(card) with the created elements
+      card.appendChild(week); //fill in the blank
+      card.appendChild(assignment1);
+      card.appendChild(assignment2);
+      card.appendChild(assignment3);
+      card.appendChild(assignment4);
+      card.appendChild(assignment5);
+  
+      cards.appendChild(card);
+    }); // end of arrow function and forEach loop
+  }
---
+++ scripts/helenaweather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDescription = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Helena&units=imperial&appid=c3b3dc02c1e7ed0006d99813ad656923';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        displayResults(data); 
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    const description = weatherData.weather[0].description.toUpperCase();
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    
+    currentTemp.innerHTML = `<p>Current Temperature: ${weatherData.main.temp.toFixed(0)} °F </p>`;
+
+    captionDescription.innerHTML = `<p>Description: ${description}</p>`;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', description);
+  }
